7e9f7ba83a3a542d4113bb5dbc04f6771c2c0a77
Jawad0Chaudhary <160344194+Jawad0Chaudhary@users.noreply.github.com>
2024-03-26 03:46:50 +0500

    Created Grading System

---
+++ main.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const marks = await inquirer.prompt([
+    {
+        name: "userinput",
+        type: "number",
+        message: "Please enter your marks: "
+    }
+]);
+if (marks.userinput >= 90 && marks.userinput < 100) {
+    console.log("Marvellous! You Got A+ Grade");
+}
+else if (marks.userinput >= 80 && marks.userinput < 90) {
+    console.log("Excellent! You Got A Grade");
+}
+else if (marks.userinput >= 70 && marks.userinput < 80) {
+    console.log("Very Good! You Got B+ Grade");
+}
+else if (marks.userinput >= 60 && marks.userinput < 70) {
+    console.log("Good! You Got B Grade");
+}
+else if (marks.userinput >= 50 && marks.userinput < 60) {
+    console.log("Fair! You Got C Grade");
+}
+else if (marks.userinput < 50) {
+    console.log("Sorry! You are Fail");
+}
+else {
+    console.log("Invalid number!");
+    console.log("(Please Try Again)");
+}
